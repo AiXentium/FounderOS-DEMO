eb2f399a49d24b1560331cee03abbbb9d05e2759
Washington Rojas <wrojas@Washingtons-MBP.lan>
2026-09-04 21:20:21 -0400

    Align OpenPage editor and settings with reference

---
+++ app/openpage/editor/page.tsx
@@ -0,0 +1,7 @@
+import { OpenPageBuilder } from '@/components/OpenPageBuilder';
+
+export const dynamic = 'force-dynamic';
+
+export default function OpenPageEditorPage() {
+  return <div className="mx-auto max-w-[1600px] px-4 py-5 md:px-6"><OpenPageBuilder initialView="editor" /></div>;
+}
